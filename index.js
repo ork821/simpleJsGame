5b60b885daed74c8a8dbba0474b80cb867ccaa14
ork821 <ork821@mail.ru>
2019-10-15 19:35:30 +0300

    creating static box and added 'click' handler

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 let start_btn = document.querySelector('#start')
 let game_filed = document.querySelector('#game')
 
+let score = 0
+
 start_btn.addEventListener('click', () => {
-    start_btn.classList.add('hide');
+    start_btn.classList.add('hide')
     game_filed.style.backgroundColor = '#fff'
     renderBox()
 })
 
+game_filed.addEventListener('click', (e) => {
+    if (e.target.dataset.box) {
+        score++
+        renderBox()
+    }
+})
+
 
 const renderBox = () => {
+    game_filed.innerHTML = ''
+    let box = document.createElement('div')
 
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = 'black'
+    box.style.top = '50px'
+    box.style.left = '50px'
+    box.style.cursor = 'pointer'
+    box.setAttribute('data-box', 'true')
+
+    game_filed.insertAdjacentElement('afterbegin', box)
 }
