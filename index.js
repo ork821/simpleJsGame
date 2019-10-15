a656038874ea0c17632c8694c5087580f7eb8845
ork821 <ork821@mail.ru>
2019-10-15 20:22:15 +0300

    Fixed problems, now we can play again

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 let start_btn = document.querySelector('#start')
 let game_filed = document.querySelector('#game')
 let time = document.querySelector('#time')
+let result = document.querySelector('#result')
+let resultHeader = document.querySelector('#result-header')
+let timeHeader = document.querySelector('#time-header')
+let input = document.querySelector('#game-time')
 
 let score = 0
 let isGameStarted = false
 
 start_btn.addEventListener('click', () => {
+    score = 0
+    resultHeader.classList.add('hide')
+    timeHeader.classList.remove('hide')
+    setGameTime()
     isGameStarted = true
     start_btn.classList.add('hide')
     game_filed.style.backgroundColor = '#fff'
@@ -36,8 +44,29 @@ game_filed.addEventListener('click', (e) => {
     }
 })
 
+input.addEventListener('change', () => {
+    time.textContent = input.value.toFixed(1)
+})
+
+
+
+const setGameScore = () => {
+    result.textContent = score.toString()
+}
+
+const setGameTime = () => {
+    time.textContent = input.value.toFixed(1)
+}
+
+
 const endGame = () => {
     isGameStarted = false
+    start_btn.classList.remove('hide')
+    game_filed.style.backgroundColor = '#ccc'
+    game_filed.innerHTML = ''
+    timeHeader.classList.add('hide')
+    resultHeader.classList.remove('hide')
+    setGameScore()
 }
 
 const renderBox = () => {
